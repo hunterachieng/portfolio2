bdb126450412f2eede7e3b422d60a37cb7f037c1
hunterachieng <achienghunter@gmail.com>
2021-08-20 08:51:32 +0300

    projects and resume

--- src/Projects.js
+++ src/Projects.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {Link }from 'react-router-dom';
-import green from './images/project1.PNG';
-import reviews from './images/project3.PNG';
-import madlibs from './images/projects.PNG';
+import chat from './images/project1.jpg';
+import todo from './images/project3.png';
+import system from './images/projects.jpg';
 
 export default function Projects(){
     return(
         <div className="Project" id="project">
             <h2>Projects</h2>
         <div className="projects">
-            <img src={green} alt = "green"/>
-            <img src={reviews} alt = "reviews"/>
-            <img src={madlibs} alt = "madlibs"/>           
+            <img src={chat} alt = "chat"/>
+            <img src={todo} alt = "todo"/>
+            <img src={system} alt = "system"/>           
         </div>
         <div className="names">
-        <Link to ={{pathname:"https://hunter-green-js.netlify.app"}}target="_blank"style={{textDecoration:"none"}}>
-            <p>E-commerce</p></Link>
-           <Link to={{pathname:"https://hunter-reviews-jsproject.netlify.app"}} target="_blank" style={{textDecoration:"none"}}>
-               <p>Customer Reviews</p></Link>
-            <Link to={{pathname:"https://hunter-madlibs-js.netlify.app"}} target="_blank" style={{textDecoration:"none"}}>
-                <p>Madlibs</p></Link>
+        <Link to ={{pathname:"https://hunters-messaging-app.netlify.app/"}}target="_blank"style={{textDecoration:"none"}}>
+            <p>Messaging App</p></Link>
+           <Link to={{pathname:"https://hunter-todo-list.netlify.app/"}} target="_blank" style={{textDecoration:"none"}}>
+               <p>To-Do List</p></Link>
+            <Link to={{pathname:"https://github.com/hunterachieng/school_system"}} target="_blank" style={{textDecoration:"none"}}>
+                <p>School System</p></Link>
         </div>
         </div>
     )
